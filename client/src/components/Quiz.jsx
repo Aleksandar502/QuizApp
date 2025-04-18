c95e0d270d6da404db9727c9c061d9a9d2f3e14a
Aleksandar_Milutinovic <63-2020@pmf.kg.ac.rs>
2025-04-18 15:47:17 +0200

    Showing questions and answers

--- client/src/components/Quiz.jsx
+++ client/src/components/Quiz.jsx
@@ -1,3 +1,27 @@
+import { useState } from "react";
+import QUESTIONS from "../questions.js";
+
 export default function Quiz() {
-  return <p>Currently active Question</p>;
+  const [userAnswers, setUserAnswers] = useState([]);
+
+  const activeQuestionIndex = userAnswers.length;
+  function handleSelectAnwser(selectedAnwser) {
+    setUserAnswers((curr) => [...curr, selectedAnwser]);
+  }
+  return (
+    <div id="quiz">
+      <div id="question">
+        <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
+        <ul id="answers">
+          {QUESTIONS[activeQuestionIndex].answers.map((anwser) => (
+            <li className="answer" key={anwser}>
+              <button onClick={() => handleSelectAnwser(anwser)}>
+                {anwser}
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 }
